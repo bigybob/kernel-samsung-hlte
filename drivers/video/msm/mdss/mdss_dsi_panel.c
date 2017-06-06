555c63472e36ea0a8c9bad2cde520a4ac5f036d7
bigybob <bigy123@seznam.cz>
2017-06-06 13:41:10 +0200

    powersuspend 1.7

--- drivers/video/msm/mdss/mdss_dsi_panel.c
+++ drivers/video/msm/mdss/mdss_dsi_panel.c
@@ -43,6 +43,10 @@ static struct mdss_dsi_driver_data msd;
 #endif	/* CONFIG_MDNIE_LITE_TUNING */
 #endif	/* CONFIG_FB_MSM_MDSS_MDP3 */
 
+#ifdef CONFIG_POWERSUSPEND
+#include <linux/powersuspend.h>
+#endif
+
 #define DT_CMD_HDR 6
 
 #define MIN_REFRESH_RATE 30
@@ -519,6 +523,10 @@ static int mdss_dsi_panel_on(struct mdss_panel_data *pdata)
 		return -EINVAL;
 	}
 
+#ifdef CONFIG_POWERSUSPEND
+	set_power_suspend_state_panel_hook(POWER_SUSPEND_INACTIVE);
+#endif
+
 	ctrl = container_of(pdata, struct mdss_dsi_ctrl_pdata,
 				panel_data);
 	mipi  = &pdata->panel_info.mipi;
@@ -566,6 +574,10 @@ static int mdss_dsi_panel_off(struct mdss_panel_data *pdata)
 
 	if (ctrl->off_cmds.cmd_cnt)
 		mdss_dsi_panel_cmds_send(ctrl, &ctrl->off_cmds);
+
+#ifdef CONFIG_POWERSUSPEND
+	set_power_suspend_state_panel_hook(POWER_SUSPEND_ACTIVE);
+#endif
 
 #if defined(CONFIG_MDSS_DSI_EVENT_HANDLER_PANEL)
 #if defined(CONFIG_FB_MSM_MDSS_MDP3)
