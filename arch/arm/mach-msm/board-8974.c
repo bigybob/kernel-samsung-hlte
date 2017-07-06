5a1100fef1c77b3fa88bfe7128a206a76cd56bd6
Gobinda Joy <gobinda.joy@gmail.com>
2017-07-06 20:50:20 +0600

    arch: arm: Fix merge! again!

--- arch/arm/mach-msm/board-8974.c
+++ arch/arm/mach-msm/board-8974.c
@@ -484,6 +484,9 @@ void __init msm_8974_reserve(void)
 	reserve_info = &msm8974_reserve_info;
 	of_scan_flat_dt(dt_scan_for_memory_reserve, msm8974_reserve_table);
 	msm_reserve();
+#ifdef CONFIG_ANDROID_PERSISTENT_RAM
+	persistent_ram_early_init(&per_ram);
+#endif
 }
 
 static void __init msm8974_early_memory(void)
